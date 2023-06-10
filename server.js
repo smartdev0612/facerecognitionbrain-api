14d92c27acf225143b2cf79a2d7e45ea66a241f2
smartdev0612 <danieldev0612@gmail.com>
2023-06-09 21:17:58 -0700

    enable user register

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
 const knex = require('knex')
 
-const postgres = knex({
+const db = knex({
   client: 'pg',
   connection: {
     host: '127.0.0.1',
@@ -13,13 +13,6 @@ const postgres = knex({
     database: 'postgres',
   },
 })
-
-postgres
-  .select('*')
-  .from('users')
-  .then((data) => {
-    console.log(data)
-  })
 
 const app = express()
 app.use(express.json())
@@ -79,17 +72,17 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body
-  bcrypt.hash(password, null, null, function (err, hash) {
-    console.log(hash)
-  })
-  database.users.push({
-    id: '125',
-    name,
-    email,
-    entries: 0,
-    joined: new Date(),
-  })
-  res.status(201).json(database.users[database.users.length - 1])
+  db('users')
+    .returning('*')
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date(),
+    })
+    .then((user) => {
+      res.json(user[0])
+    })
+    .catch((err) => res.status(400).json('unable to join'))
 })
 
 app.get('/profile/:id', (req, res) => {
