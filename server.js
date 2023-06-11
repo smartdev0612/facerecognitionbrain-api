7a997f5c33215c367bba2eb3a84921d15859ece9
smartdev0612 <danieldev0612@gmail.com>
2023-06-11 13:34:07 -0700

    create endpoints for signin

--- server.js
+++ server.js
@@ -18,71 +18,58 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-}
-
 app.get('/', (req, res) => {
-  res.status(200).json(database.users)
+  res.send('success')
 })
 
 app.post('/signin', (req, res) => {
-  bcrypt.compare(
-    'apples',
-    '$2a$10$C0LpC3umiT1wXgVxVAGVgOSH0b4Hmei9.QPtWc15b0iXed.1Ihj0q',
-    function (err, res) {
-      console.log('first guess', res)
-    }
-  )
-
-  bcrypt.compare(
-    'veggies',
-    '$2a$10$C0LpC3umiT1wXgVxVAGVgOSH0b4Hmei9.QPtWc15b0iXed.1Ihj0q',
-    function (err, res) {
-      console.log('second guess', res)
-    }
-  )
-
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0])
-  } else {
-    res.status(400).json('error logging in')
-  }
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', req.body.email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+      if (isValid) {
+        return db
+          .select('*')
+          .from('users')
+          .where('email', '=', req.body.email)
+          .then((user) => {
+            res.json(user[0])
+          })
+          .catch((err) => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch((err) => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body
-  db('users')
-    .returning('*')
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user[0])
-    })
-    .catch((err) => res.status(400).json('unable to join'))
+  const hash = bcrypt.hashSync(password)
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into('login')
+      .returning('email')
+      .then((loginEmail) => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0])
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+  }).catch((err) => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
@@ -109,7 +96,7 @@ app.put('/image', (req, res) => {
     .increment('entries', 1)
     .returning('entries')
     .then((entries) => {
-      res.json(entries[0])
+      res.json(entries[0].entries)
     })
     .catch((err) => res.status(400).json('unable to get entries'))
 })
